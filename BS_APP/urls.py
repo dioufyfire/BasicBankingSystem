972a7de12f966e66acc31b201bc3e32083895422
Mamadou DIOUF DSI/A2I/DBA <mamadou.diouf6@orange-sonatel.com>
2023-01-23 23:46:15 +0000

    Update urls.py

--- BS_APP/urls.py
+++ BS_APP/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('users/',user),
     path('transfer/<int:pk>',transfer),
     path('add_transfer/<int:pk>',add_transfer),
+    path('add_amount/<int:pk>',add_amount),
     path('history/',history)
 ]
